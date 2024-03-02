cc5e1726f1bb33f1bfe27023d5f3cfca023cdfd0
Kru301019 <krujoshi3010@gmail.com>
2024-03-02 14:04:05 +0000

    ++

--- index.js
+++ index.js
@@ -2,3 +2,37 @@
 /**
  * Add your functions here...
  */
+
+// function targetTextToConsole(e) {
+//     const out = e.target.textContent;
+//     console.log(out);
+// };
+
+// function tttcAttacher() {
+//     myButton = document.querySelector('#button0');
+//     myButton.addEventListener('click', targetTextToConsole);
+// };
+
+// tttcAttacher();
+
+function lovelyToggle() {
+    // Get the paragraph element
+    var para = document.getElementById('thisisalovelyparagraph');
+
+    // Check the current color of the paragraph text
+    if (para.style.color == 'red') {
+        // If the color is currently red, change it to blue
+        para.style.color = 'blue';
+    } else {
+        // If the color is not currently red (including if it's not set at all), change it to red
+        para.style.color = 'red';
+    }
+}
+
+
+function lovelyParaAttacher() {
+    
+    const button_1 = document.querySelector('#button1');
+    button_1.addEventListener('click', lovelyToggle);
+};
+
